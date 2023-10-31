bfac005a452a263013496977125877313a26cd32
isabeldonner <isabel.donner@yahoo.com>
2023-10-31 01:36:43 -0400

    Isabel's encode and main

--- isabels_encode.py
+++ isabels_encode.py
@@ -1,8 +1,13 @@
 def encode(password):
   encoded_pass = ""
   for digit in password:
-    digit = int(digit) + 3
-    encoded_pass = encoded_pass + str(digit)
+    if int(digit) >= 7:
+      digit = str((int(digit) + 3) % 10)
+      encoded_pass += digit
+    else:
+      digit = int(digit) + 3
+      encoded_pass = encoded_pass + str(digit)
+  return encoded_pass
 def display_menu():
   global password
   print('Menu')
@@ -10,7 +15,7 @@ def display_menu():
   print('1. Encode')
   print('2. Decode')
   print('3. Quit')
-  selection = int(input('Please enter an option: ')
+  selection = int(input('Please enter an option: '))
   while selection != 3:
     if selection == 1:
       password = input('Please enter your password to encode: ')
@@ -19,7 +24,8 @@ def display_menu():
       display_menu()
     elif selection == 2:
       decoded_pass = decode(password)
-      print('The encoded password is' , encoded_pass, ', and the original password is' , decoded_pass)
+      print('The encoded password is', encoded_pass, ', and the original password is', decoded_pass)
+
 if __name__ == "__main__":
   display_menu()
 
