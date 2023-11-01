5f2dfedfacd25d2a4b2bc3c01c9364a866305b73
thomasfitzpa <thomas.fitzpatri@ufl.edu>
2023-10-31 20:23:32 -0400

    new code

--- isabels_encode.py
+++ isabels_encode.py
@@ -5,28 +5,36 @@ def encode(password):
       digit = str((int(digit) + 3) % 10)
       encoded_pass += digit
     else:
-      digit = int(digit) + 3
-      encoded_pass = encoded_pass + str(digit)
+      digit = str(int(digit) + 3)
+      encoded_pass = encoded_pass + digit
   return encoded_pass
+
+def decode(password):
+  decoded_pass = ""
+  for digit in password:
+    if int(digit) < 3:
+      decoded_pass += str((int(digit) + 7))
+    else:
+      decoded_pass += str(int(digit) - 3)
+  return decoded_pass
+
 def display_menu():
-  global password
-  print('Menu')
-  print('-------------')
-  print('1. Encode')
-  print('2. Decode')
-  print('3. Quit')
-  selection = int(input('Please enter an option: '))
+  selection = 0
   while selection != 3:
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit\n')
+    selection = int(input('Please enter an option: '))
     if selection == 1:
       password = input('Please enter your password to encode: ')
       encoded_pass = encode(password)
-      print('Your password has been encoded and stored!')
-      display_menu()
+      print('Your password has been encoded and stored!\n')
     elif selection == 2:
-      decoded_pass = decode(password)
-      print('The encoded password is', encoded_pass, ', and the original password is', decoded_pass)
+      decoded_pass = decode(encoded_pass)
+      print('The encoded password is', encoded_pass, ', and the original password is', decoded_pass, '\n')
+
 
 if __name__ == "__main__":
   display_menu()
-
-  
